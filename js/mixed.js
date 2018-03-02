d816d2eed261feaa1ac7da6af2dc9200c5b327fb
Unknown <1079096510@qq.com>
2018-03-02 23:14:35 +0800

    2018 周五晚上
    mac

--- js/mixed.js
+++ js/mixed.js
@@ -15,7 +15,7 @@ layui.use('flow', function() {
     var count = picts.data.length - 1;
 
     flow.load({
-        elem: '.layui-tab-item', //指定列表容器
+        elem: '.mixed-box', //指定列表容器
         isAuto: true,
         end: '没有更多图片啦，不要再往下拉了，求求你了',
         done: function(page, next) { //到达临界点（默认滚动触发），触发下一页
@@ -28,23 +28,32 @@ layui.use('flow', function() {
                 if (count == -1) {
                     lis.push('<div class="mixed-pic-box shadow animated zoomIn"> ' +
                         '<div class = "mixed-pics" >' +
-                        '<a>' +
-                        '<img src = "images/pic/6.jpg" alt = "" srcset = "" > </a> </div > <p class = "mixed-name" >' +
-                        '红花 </p> <hr> <div class = "mixed-btns" > <span class = "layui-btn layui-btn-primary layui-btn-xs" ><i class = "fa fa-eye fa-fw" > </i>查看 </span > <span class = "layui-btn layui-btn-primary layui-btn-xs" >' +
+                        '<a >' +
+                        '<img src = "images/pic/0.jpg" alt = "" srcset = "" > </a> </div > <p class = "mixed-name" >' +
+                        '最后一张是为空哦 </p> <hr> <div class = "mixed-btns" > <span class = "layui-btn layui-btn-primary layui-btn-xs" ><i class = "fa fa-eye fa-fw" > </i>查看 </span > <span class = "layui-btn layui-btn-primary layui-btn-xs" >' +
                         '<i class = "fa fa-download fa-fw" > </i>下载 </span > </div></div > ');
                 } else {
                     lis.push('<div class="mixed-pic-box shadow animated zoomIn"> ' +
                         '<div class = "mixed-pics" >' +
-                        '<a>' +
-                        '<img src = "' + picts.data[count].src + '" alt = "' + picts.data[count].alt + '" srcset = "" > </a> </div > <p class = "mixed-name" >' +
-                        '红花 </p> <hr> <div class = "mixed-btns" > <span class = "layui-btn layui-btn-primary layui-btn-xs" > <i class = "fa fa-eye fa-fw" ></i>查看 </span > <span class = "layui-btn layui-btn-primary layui-btn-xs" >' +
+                        '<a href="javascript:view('+ count+')">' +
+                        '<img src = "' + picts.data[count].src + '" layer-src="' + picts.data[count].src + '" alt = "' + picts.data[count].alt + '" srcset = "" > </a> </div > <p class = "mixed-name" >' +
+                        picts.data[count].alt + '</p> <hr> <div class = "mixed-btns" > <span class = "layui-btn layui-btn-primary layui-btn-xs" > <i class = "fa fa-eye fa-fw" ></i>查看 </span > <span class = "layui-btn layui-btn-primary layui-btn-xs" >' +
                         '<i class = "fa fa-download fa-fw" > </i>下载 </span > </div></div > ');
                 }
                 count--;
             }
 
-            next(lis.join(''), page < picts.data.length / 8);
+            next(lis.join(''), page < picts.data.length/8);
 
         }
     });
+
+    function view() {
+        layer.photos({
+            photos: '.mixed-pics>a'
+            ,anim: 5 //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
+          }); 
+      }
+
+
 });
